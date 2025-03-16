8d846174d20ec2496086cc54de17b80de0ccdf1e
Hassan Farag <hassanfarag@Hassans-MacBook-Air.local>
2025-03-16 21:27:01 +0100

    added ami

--- aws/aws_infra.tf
+++ aws/aws_infra.tf
@@ -213,24 +213,9 @@ resource "aws_security_group" "db" {
   
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "app" {
-  ami = data.aws_ami.ubuntu.id # Ubuntu 24.04 LTS
+  ami = "ami-0c1ac8a41498c1a9c" # Ubuntu 24.04 LTS
   instance_type = "t3.micro"
   subnet_id = aws_subnet.app_subnet.id
   vpc_security_group_ids = [aws_security_group.app.id]
@@ -241,7 +226,7 @@ resource "aws_instance" "app" {
 }
 
 resource "aws_instance" "db" {
-  ami = data.aws_ami.ubuntu.id # Ubuntu 24.04 LTS
+  ami = "ami-0c1ac8a41498c1a9c" # Ubuntu 24.04 LTS
   instance_type = "t3.micro"
   subnet_id = aws_subnet.db_subnet.id
   vpc_security_group_ids = [aws_security_group.db.id]
